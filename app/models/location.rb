e2105a4563078362ccf74bcfcbdaa6276a870420
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-09-18 12:20:04 -0600

    Linking Locations with their proper parents

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,13 @@
 class Location < ActiveRecord::Base
+  validates :name, presence: true
 
   def self.types
     ['Country', 'Province', 'City', 'District']
   end
   
+  def parent
+  	Location.types.each { |t| return t if self.respond_to? t.underscore.to_sym }
+  	nil
+  end
+
 end
